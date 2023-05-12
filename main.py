473957049a4bacab58fddb251bed1a8aad1a8f0a
Ishaaq Ollite <iollite@hotmail.com>
2023-05-12 20:32:43 +0100

    undid stuff

--- main.py
+++ main.py
@@ -1,145 +1,101 @@
+import pygame
 import os
 import random
 import sys
-import time
 
-import pygame
-import random
-import time
-
-
-
-
-def load_animation(direction, state):
-    """
-        Loads the animation for the given direction and state
-    """
-    animation_file = "./assets/RPGMCharacter_v1.0/pngs/{DIRECTION}_{STATE}.png"
-
-    return pygame.image.load(animation_file.format(STATE=state, DIRECTION=direction))
-
-current_animation_state = "idle"
-current_animation_direction = "right"
-last_animation_time = 0
-last_animation_state = "idle"
-last_animation_direction = "up"
+class Player:
+    def __init__(self, name, health):
+        
+        self.name = name
+        self.health = health
 
 
-def split_png(png_file):
-    """Splits a png into multiple sections. Used for spirte sheets"""
+        # Set the player's animation state to idle and direction to right
 
-
-    # Load the image
-    image = pygame.image.load(png_file)
-
-    # Get the width and height of the image
-    image_width = image.get_width()
-    image_height = image.get_height()
-
-    # Get the number of rows and columns
-    rows = image_height // 32
-    columns = image_width // 32
-
-    # Create a list to store the images
-    images = []
-
-    # Loop through the rows and columns
-    for row in range(rows):
-        for column in range(columns):
-            # Create a new image
-            new_image = pygame.Surface((32, 32))
-
-            # Copy the sprite from the sheet onto the new image
-            new_image.blit(image, (0, 0), (column * 32, row * 32, 32, 32))
-
-            # Add the image to the list
-            images.append(new_image)
-
-    return images
-
-
+        self.current_animation_frame = 0   
+        self.current_animation_state = "idle"   
+        self.current_animation_direction = "up"  
     
-
-
-def play_animation():
-    """
-        Draws the character on the screen
-    """
-    global current_animation_state
-    global current_animation_direction
-    global current_time
-
-    global last_animation_time
-    global last_animation_state
-    global last_animation_direction
-
-
-    possible_states = ["idle", "walk", "attack"]
-    possible_directions = ["left", "right", "up", "down"]
-
-    # if last_animation_time
-
-    if current_time - last_animation_time > 1000: # 1 second
-        # Move to the next state
-        current_animation_state = possible_states.index(last_animation_state) + 1 # find out what index the last state was in the list amd move to the next one
-        current_animation_state = possible_states[current_animation_state % len(possible_states)] # make sure the index is in range
-
-        # Move to the next direction
-        current_animation_direction = possible_directions.index(last_animation_direction) + 1 # find out what index the last direction was in the list amd move to the next one
-        current_animation_direction = possible_directions[current_animation_direction % len(possible_directions)] # make sure the index is in range
-
-        # Move the current states to the last states a new animation will be played
-        last_animation_time = current_time
-        last_animation_state = current_animation_state
-        last_animation_direction = current_animation_direction
-
-    image = load_animation(current_animation_direction, current_animation_state)
-
-    # make the image bigger
-    image = pygame.transform.scale(image, (image.get_width() * 4, image.get_height() * 4))
-
-
-
-    game_display.blit(image, (0, 0))
+        self.last_animation_time = 0
+        self.last_animation_state = "idle"
+        self.last_animation_direction = "up"
+        
 
 
 
 
 
+class Animations:
+    def __init__(self) -> None:
+        pass
 
-# Set up the display
-display_width = 800
-display_height = 600
-game_display = pygame.display.set_mode((display_width, display_height))
-
-# Load the image
-image = load_animation(current_animation_direction, current_animation_state)
-
-# Set up the clock
-clock = pygame.time.Clock()
-
-# Main game loop
-while True:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+    def split_image(image_path :str, x_pixels_split :int = 64, y_pixels_split :int = 64) -> list:
+        """ Takes an image and splits it into a list of smaller images based on the x and y pixels 
+        split. For example, a 256x128 image split into 64x64 images would return a list of 8 images """
 
 
-    print("STATE: {STATE}, DIRECTION: {DIRECTION}".format(STATE=current_animation_state, DIRECTION=current_animation_direction))
+        full_image = pygame.image.load(image_path).convert_alpha()
 
-    current_time = pygame.time.get_ticks()
+        # Get the width and height of the full image
+        full_image_width = full_image.get_width()
+        full_image_height = full_image.get_height()
+
+        # Get the number of rows and columns
+        rows = full_image_height // y_pixels_split
+        columns = full_image_width // x_pixels_split
+
+        # Create a list to store the images
+        split_images = []
+
+        # Loop through the rows and columns
+        for row in range(rows):
+            for column in range(columns):
+
+                # Create a new image
+                new_image = pygame.Surface((x_pixels_split, y_pixels_split))
+
+                # Replace the background color with a transparent color
+                new_image.fill((255, 255, 255))
+                new_image.set_colorkey((255, 255, 255))
+
+                # Copy the sprite from the sheet onto the new image
+                new_image.blit(full_image, (0, 0), (column * x_pixels_split, row * y_pixels_split, x_pixels_split, y_pixels_split))
+
+                # Add the image to the list
+                split_images.append(new_image)
+
+        return split_images
     
+    def get_animation_frame(self, animation_state :str, animation_direction :str, animation_frames :list, animation_speed :int = 100) -> pygame.Surface:
+        """ Returns the next animation frame based on the animation state, direction, and speed """
 
-    # Clear the screen
-    game_display.fill((255, 255, 255))
+        # Get the current time
+        current_time = pygame.time.get_ticks()
 
-    play_animation()
+        # Check if the animation state or direction has changed
+        if animation_state != self.last_animation_state or animation_direction != self.last_animation_direction:
 
+            # Reset the animation frame
+            self.current_animation_frame = 0
 
-    # Update the display
-    pygame.display.update()
+            # Update the last animation state and direction
+            self.last_animation_state = animation_state
+            self.last_animation_direction = animation_direction
 
-    # Set the frame rate
-    clock.tick(60)
+        # Check if enough time has passed to update the animation frame
+        elif current_time - self.last_animation_time > animation_speed:
+
+            # Update the animation frame
+            self.current_animation_frame += 1
+
+            # Reset the animation frame if the animation has reached the end
+            if self.current_animation_frame >= len(animation_frames):
+                self.current_animation_frame = 0
+
+            # Update the last animation time
+            self.last_animation_time = current_time
+
+        # Return the current animation frame
+        return animation_frames[self.current_animation_frame]
+    
+    
---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+import random
+import sys
+import time
+
+import pygame
+import random
+import time
+import main
+
+
